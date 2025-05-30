37ce16e2c38abcb8110da7bc47c30fc19d603a6c
jibin jose <jibinjose884@gmail.com>
2025-05-30 15:14:01 +0530

    Initial commit of React ToDo App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
+  const handleDelete = (id) => {
+    setToDos(toDos.filter((item) => item.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() => {
+            if (toDo.trim() !== '') {
+              setToDos([...toDos, { id: Date.now(), text: toDo }]);
+              setToDo('');
+            }
+          }}
+          className="fas fa-plus"
+        ></i>
+      </div>
+      <div className="todos">
+        {toDos.map((item) => (
+          <div className="todo" key={item.id}>
+            <div className="left">
+              <p>{item.text}</p>
+            </div>
+            <div className="right">
+              <i
+                className="fas fa-times"
+                onClick={() => handleDelete(item.id)}
+              ></i>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
